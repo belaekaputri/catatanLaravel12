5421467eeba951c2031899629b787d7c143d34c4
belaekaputri <belaekaputri28@gmail.com>
2025-10-28 13:45:34 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,36 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.beranda');
 });
+
+
+Route::get('/about', function () {
+    $data = [
+        'nama' => 'Bela Eka Putri',
+        'umur' => 28,
+        'alamat' => 'Indonesia'
+    ];
+    return view('pages.about', $data); //About.blade.php di views
+    //atau
+    /*
+    return view('pages.about',  [
+        'nama' => 'Bela Eka Putri',
+        'umur' => 28,
+        'alamat' => 'Indonesia'
+    ];)
+    */
+});
+
+
+Route::get('/about/{id}/detail', function ($id) {
+    return view('pages.detail', [
+        'nama' => 'Bela Eka Putri',
+        'umur' => 28,
+        'alamat' => 'Indonesia',
+        'nomor' => $id
+    ]);
+});
+
+Route::view('/contact', 'pages.contact');
+//jika ada yang akses /contact,arahkan ke views>folder Pages> contact.blade.php
