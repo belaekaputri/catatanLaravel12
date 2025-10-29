88a67eace15708d930d48205ded9d9c8006a1457
belaekaputri <belaekaputri28@gmail.com>
2025-10-29 13:57:53 +0700

    model

---
+++ app/Models/produk.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class produk extends Model
+{   //inisiasi tabel produk
+    protected $table = 'tb_produk';
+
+    //inisiasi primary key dalam tabel
+    protected $primaryKey = 'id_produk';
+
+
+    //data apa yan bisa kita isiikan ke tabel inisiasasi data yang dapat kita isi
+    protected $fillable = ['nama_produk', 'harga', 'stok'];
+
+
+
+    //insiasi data yang tida boleh kita isi
+    protected $guarded = ['id_produk'];
+}
